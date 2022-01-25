d6b22735cb473c7e990dcdc185c250336b37b4eb
sadafkhan0107 <66263424+sadafkhan0107@users.noreply.github.com>
2022-01-25 16:52:43 +0530

    Add files via upload
    Question link- https://www.hackerrank.com/challenges/phone-book/problem?isFullScreen=true

---
+++ HashMap in java.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+	public static void main(String []argh)
+	{
+        HashMap <String, Integer> map= new HashMap<>();
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+            map.put(name, phone);
+			in.nextLine();
+		}
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            if(map.containsKey(s))
+             System.out.println(s+"="+map.get(s));
+            else
+             System.out.println("Not found");
+		}
+	}
+}
+
+
+
